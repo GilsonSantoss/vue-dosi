c787ba087ee9a452d6d7bea8a3eff47969ffc257
Gilson Santos <gilsonjosert@gmail.com>
2019-10-17 04:18:34 +0000

    Add HomePage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
     <h1>Dosi - Reconstrucao</h1>
+    <HomePage />
   </div>
 </template>
+
+<script>
+const Home = () => import('./pages/Home')
+
+export default {
+  components: {
+    HomePage: Home
+  }
+}
+</script>
