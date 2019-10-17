ea5d4639b86d2c2857d266afcd43c3761406775c
Gilson Santos <gilsonjosert@gmail.com>
2019-10-17 05:00:53 +0000

    Install eslint / Reset css

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,60 +1,22 @@
 <template>
-  <transition name="fade" apper>
-    <div @click="classActive = !classActive" :class="{active: classActive}">
-      <h1>{{ title }}</h1>
-    </div>
-  </transition>
+  <section>
+    Home
+  </section>
 </template>
 
 <script>
-export default {
-
-  data () {
-    return {
-      title: 'Home Page',
-      classActive: true
-    }
-  },
-  methods: {
-    tabClick () {
-      alert('Teste')
-    }
-  }
-
-}
+export default { }
 </script>
 
 <style lang="scss" scoped>
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
 
-  div {
-    background:#333;
-    user-select: none;
-    border-radius:8px;
-    box-sizing: border-box;
-    padding:16px;
-
-    h1{
-      margin:0;
-      padding:0;
-      font-family:'Roboto';
-      color:#EEE;
-    }
-  }
-
-  .active{
-    background:#229933;
-  }
-
-  // Animation tag to transition Vue
-  .fade-enter-active {
-    transition: all .3s ease;
-  }
-  .fade-leave-active {
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-  }
-  .fade-enter, .fade-leave-to{
-    transform: translateX(10px);
-    opacity: 0;
+  section{
+    font-family: Roboto;
+    width:100%;
+    height:100%;
+    margin:0;
+    padding:0;
+    background:#EFEFEF;
   }
 </style>
